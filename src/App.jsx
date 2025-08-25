24edffaafbb1fe552a2ed12004b57fb3d4bdb89e
Tulio Martins <tuliomnatividade@outlook.com>
2025-08-25 17:19:35 -0300

    Update App.jsx
    atualizado link da geosol no icon da geopar

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://www.geosol.com.br/" target="_blank">
           <img src={geoparLogo} className="logo geopar" alt="React logo" />
         </a>
       </div>
